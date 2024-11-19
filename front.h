89db46f6d3ed6743f295504a80b26192afd02f72
Hanns3 <128602036+Hanns3@users.noreply.github.com>
2024-11-19 22:10:03 +0200

    Update front.h

--- front.h
+++ front.h
@@ -5,8 +5,6 @@
 
 class Front {
 public:
-    // todo: How use virtual InitializeSkin same for back and front, front needs rank and suit
-    // todo: implement setter
     Front(const QString& rank, const QString& suit, const QString& basePath);
     void InitializeSkin(const QString& basePath);
     QPixmap getFront() const;
